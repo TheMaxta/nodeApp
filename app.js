5e13f366eb471d17fbda8932f1120b056c20c04e
Maximillian Mahlke <themaxta@gmail.com>
2022-05-23 15:02:35 -0600

    setup stories index page and added route

--- app.js
+++ app.js
@@ -20,14 +20,28 @@ connectDB()
 
 const app = express()
 
+//Body Parser
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
 //Logging
 if (process.env.NODE_ENV === "development"){
     app.use(morgan('dev'))
 }
 
+//Handlebars Helpers
+const { formatDate } = require('./helpers/hbs.js')
+
+
 //Handlebars
-app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs'}));
-app.set('view engine', '.hbs');
+app.engine('.hbs', exphbs.engine({ 
+  helpers: { 
+    formatDate, 
+  },
+  defaultLayout: 'main',
+   extname: '.hbs'}
+   ));
+app.set('view engine', '.hbs')
 
 //Sessions
 app.use(session({
--- routes/stories.js
+++ routes/stories.js
@@ -15,4 +15,43 @@ router.get('/add', ensureAuth, (req, res) => {
      
 })
 
+
+// @desc      Process add Form
+// @route     POST /stories
+//create a route
+router.post('/', ensureAuth, async (req, res) => {
+    try {
+        req.body.user = req.user.id
+        await Story.create(req.body)
+        res.redirect('/dashboard')
+    } catch (err) {
+        console.error(err)
+        res.render('/error/500')
+    }
+     
+})
+
+
+// @desc      Show All Stories 
+// @route     GET /stories/add
+//create a route
+router.get('/', ensureAuth, async (req, res) => {
+    try {
+        const stories = await Story.find({ status: 'public'})
+        
+            //.populate('user')
+            .sort({createdAt: 'desc' })
+            .lean()
+
+            res.render('stories/index', {
+                stories
+            })
+    } catch (err) {
+        console.error(err)
+        res.render('/error/500')
+    }
+     
+})
+
+
 module.exports = router  
