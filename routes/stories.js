ec857b296bd86f9a5fce86fb282cfb4b8bc46929
Maximillian Mahlke <themaxta@gmail.com>
2022-05-31 11:09:57 -0600

    added helper for selecting proper attribute on form from drop down

--- routes/stories.js
+++ routes/stories.js
@@ -52,8 +52,23 @@ router.get('/', ensureAuth, async (req, res) => {
 
 // @desc      Edit Stories
 // @route     GET /stories/edit/:id
-router.get('/', ensureAuth, async (req, res) => {
-    
+router.get('/edit/:id', ensureAuth, async (req, res) => {
+    const story = await Story.findOne({
+        _id: req.params.id
+    }).lean()
+
+    if (!story){
+        res.render('/error/404')
+    }
+
+    if(story.user != req.user.id){
+        res.redirect('/stories')
+    } else {
+        res.render('stories/edit', {
+            story,
+        })
+    }
+
 })
 
 module.exports = router  
