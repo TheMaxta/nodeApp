4f8ca0bb71b0efa96675170d4bb7813f4b7591df
Maximillian Mahlke <themaxta@gmail.com>
2022-05-31 12:09:01 -0600

    added put override and users can now edit posts

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 const morgan = require('morgan')
 const passport = require('passport')
 const session = require('express-session')
@@ -23,6 +24,16 @@ const app = express()
 //Body Parser
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+
+//method override for put
+app.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    let method = req.body._method
+    delete req.body._method
+    return method
+  }
+}))
 
 //Logging
 if (process.env.NODE_ENV === "development"){
