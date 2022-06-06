3dde0ea0a4786ff719159fd6ae25dbdaf2538fef
Maximillian Mahlke <themaxta@gmail.com>
2022-06-06 16:37:57 -0600

    single user story page added

--- routes/stories.js
+++ routes/stories.js
@@ -55,9 +55,10 @@ router.get('/', ensureAuth, async (req, res) => {
 router.get('/:id', ensureAuth, async (req, res) => {
     try {        
         const story = await Story.findById( { _id: req.params.id})
-        .lean()
         .populate('user')
-        
+
+        .lean()
+
         if (!story){
             res.render('/error/404')
         } else {
@@ -66,10 +67,34 @@ router.get('/:id', ensureAuth, async (req, res) => {
             })
         }
     } catch (err) {
-        console.render(err)
+        console.error(err)
         res.render('error/404')
     }
 })
+
+// @desc      Show Users Stories/y
+// @route     GET /stories/:userId
+router.get('/user/:userId', ensureAuth, async (req, res) => {
+    try {
+        const stories = await Story.find({ 
+            user: req.params.userId,
+            status: 'public'
+        })
+
+            .populate('user')
+            .sort({ createdAt: 'desc' })
+            .lean()
+
+            res.render('stories/index', {
+                stories
+            })
+    } catch (err) {
+        console.error(err)
+        res.render('/error/500')
+    }
+     
+})
+
 
 // @desc      Edit Stories
 // @route     GET /stories/edit/:id
