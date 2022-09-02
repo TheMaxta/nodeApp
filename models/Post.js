153e28201e63eefca83a80b7052017782d9f44bb
Maximillian Mahlke <themaxta@gmail.com>
2022-09-02 13:03:54 -0600

    added replacement for sotry

---
+++ models/Post.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+const PostSchema = new mongoose.Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    body:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    status:{
+        type: String,
+        default: 'public',
+        enum: ['public', 'private']
+    },
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    comments: [mongoose.Schema.Types.ObjectId],
+    
+    score:{
+        type: Number,
+        default: 0
+    }
+
+
+})
+
+module.exports = mongoose.model('Post', PostSchema)
