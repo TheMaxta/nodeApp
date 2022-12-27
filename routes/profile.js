4663f6b5205c1c73e38ead5d0667842f35c136b0
Maximillian Mahlke <themaxta@gmail.com>
2022-12-26 18:02:54 -0700

    minor visual tweaks

--- routes/profile.js
+++ routes/profile.js
@@ -70,6 +70,47 @@ router.get('/test', ensureAuth, async (req, res) => {
 })
 
 
+router.get('/messages', ensureAuth, async (req, res) => {
+    try {
+        const posts = await Post.find({ user: req.user.id }).lean()
+        const oldestPost = posts[0]
+        const newestPost = posts[posts.length-1]
+        let highestPost = posts[0];
+        let lowestPost = posts[0];
+        let totalNumPosts = posts.length;
+
+
+        posts.forEach(function (currentPost, index, arr) {
+            if (currentPost.score > highestPost.score){ highestPost = currentPost }
+        });
+        posts.forEach(function (currentPost){
+            if (currentPost.score < lowestPost.score){ lowestPost = currentPost }
+        });
+
+
+        res.render('messagesPage', {
+            layout: 'other',
+            name: req.user.firstName,
+            lastName: req.user.lastName,
+            createdAt: req.user.createdAt,
+            image: req.user.image,
+            posts,
+            oldestPost,
+            newestPost,
+            highestPost,
+            lowestPost,
+            totalNumPosts
+            
+        })    
+    } catch (err) {
+        console.error(err)
+        res.render('error/500')        
+    }
+})
+
+
+
+
 
 
 
